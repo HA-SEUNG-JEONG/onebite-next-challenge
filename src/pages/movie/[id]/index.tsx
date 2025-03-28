bfc1b6e853b60bf269c97574fb073de11558f747
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-28 20:21:48 +0900

    feat: 영화 상세 페이지에서 서버 사이드 렌더링을 통한 영화 데이터 가져오기 기능 추가

--- src/pages/movie/[id]/index.tsx
+++ src/pages/movie/[id]/index.tsx
@@ -1,34 +1,51 @@
-import { useRouter } from "next/router";
 import React from "react";
-import dummyData from "@/dummy.json";
 import Image from "next/image";
+import { GetServerSidePropsContext } from "next";
+import { MovieData } from "@/types";
 
-const Detail = () => {
-    const router = useRouter();
-    const { id } = router.query;
-    const movie = dummyData.find((movie) => movie.id === Number(id));
+export const getServerSideProps = async (
+    context: GetServerSidePropsContext
+) => {
+    try {
+        const response = await fetch(
+            `http://localhost:12345/movie/${context.params?.id}`
+        );
+        if (!response.ok) {
+            throw new Error("네트워크 오류");
+        }
+        const movieResult = await response.json();
+        return {
+            props: { movieResult }
+        };
+    } catch {
+        return {
+            notFound: true
+        };
+    }
+};
 
+const Detail = ({ movieResult }: { movieResult: MovieData }) => {
     return (
         <div className="p-4">
             <div className="flex gap-4 justify-center mb-12">
                 <div className="relative aspect-[2/3] w-48 overflow-hidden rounded-lg">
                     <Image
-                        src={movie?.posterImgUrl ?? ""}
-                        alt={movie?.title ?? ""}
+                        src={movieResult?.posterImgUrl ?? ""}
+                        alt={movieResult?.title ?? ""}
                         fill
                         className="object-cover transition-transform w-32 h-32"
                     />
                 </div>
             </div>
-            <h4 className="mb-4 text-2xl font-bold">{movie?.title}</h4>
+            <h4 className="mb-4 text-2xl font-bold">{movieResult?.title}</h4>
             <div className="flex flex-col gap-2">
                 <p className="text-sm ">
-                    {movie?.releaseDate} / {movie?.genres.join(", ")} /{" "}
-                    {movie?.runtime}분
+                    {movieResult?.releaseDate} /{" "}
+                    {movieResult?.genres.join(", ")} / {movieResult?.runtime}분
                 </p>
-                <p className="text-lg mb-4">{movie?.company}</p>
-                <p className="text-lg font-bold">{movie?.subTitle}</p>
-                <p className="text-lg">{movie?.description}</p>
+                <p className="text-lg mb-4">{movieResult?.company}</p>
+                <p className="text-lg font-bold">{movieResult?.subTitle}</p>
+                <p className="text-lg">{movieResult?.description}</p>
             </div>
         </div>
     );
