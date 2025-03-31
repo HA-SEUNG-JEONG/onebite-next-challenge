242e05540bf85e2aaa32731efe59b8e419ce52c8
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-31 15:15:40 +0900

    fix : 이미지 도메인 설정을 remotePatterns로 변경하여 보안 강화

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ["media.themoviedb.org"]
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "media.themoviedb.org"
+            }
+        ]
     }
 };
 
