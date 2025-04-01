1c5eb3d7179e5db6045e2c24813fade495a3200b
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-04-01 16:03:09 +0900

    fix : 썸네일 주소 수정

--- src/pages/movie/[id]/index.tsx
+++ src/pages/movie/[id]/index.tsx
@@ -62,7 +62,10 @@ const Detail = ({
         <div className="p-4">
             <Head>
                 <title>{movieResult.title} | 한입 씨네마</title>
-                <meta property="og:image" content={movieResult.posterImgUrl} />
+                <meta
+                    property="og:image"
+                    content={`https://onebite-cinema-otbg6c1lm-haseungjeongs-projects.vercel.app/movie/${movieResult.id}`}
+                />
                 <meta
                     property="og:title"
                     content={`${movieResult.title} | 한입 씨네마에 등록된 영화들을 살펴보세요.`}
